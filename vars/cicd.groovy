e9afeaab8045f2922551d80673ecddc5a732d369
guruvinayaws <guruvinayaws@gmail.com>
2025-12-08 12:02:26 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,10 +1,21 @@
 def gitDownload(repo)
 {
- "https://github.com/guruvinayaws/${repo}.git"
+    git "https://github.com/IntelliqDevops/${repo}.git"
+
 }
 
 def buildArtifact()
 {
-  sh 'mvn package'
+     sh 'mvn package'
+
 }
 
+def deployTomcat(ipaddress,context)
+{
+    sh "scp /var/lib/jenkins/workspace/${JOB_NAME}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${context}.war"
+}
+
+def runSelenium()
+{
+     sh "java -jar /var/lib/jenkins/workspace/${JOB_NAME}/testing.jar"
+}
